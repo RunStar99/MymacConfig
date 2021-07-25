d67ecdb8f841049854a34933293b559316ca8a9a
runstar99 <run.starlamp@gmail.com>
2021-07-25 20:04:06 +0800

    update

---
+++ openwrt-start.sh
@@ -0,0 +1,28 @@
+/usr/sbin/balethirq.pl
+/etc/init.d/AdGuardHome start
+sleep 60 && rclone mount emby: /home/gdrive \
+--umask 0000 \
+--default-permissions \
+--no-check-certificate \
+--allow-non-empty \
+--allow-other \
+--vfs-cache-mode writes \
+--vfs-read-chunk-size 32M \
+--vfs-read-chunk-size-limit 256M \
+--buffer-size 128M \
+--use-mmap \
+--daemon &
+ rclone mount gdrive2: /mnt/mmcblk1p3/docker/emby/share2 \
+--umask 0000 \
+--default-permissions \
+--no-check-certificate \
+--allow-non-empty \
+--allow-other \
+--vfs-cache-mode writes \
+--vfs-read-chunk-size 32M \
+--vfs-read-chunk-size-limit 256M \
+--buffer-size 128M \
+--use-mmap \
+--daemon &
+
+exit 0
